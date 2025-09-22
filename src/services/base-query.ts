74a1f963de0f960be881f042bc2fdcfbaad91a26
ilhamraff <ifadhilah261@gmail.com>
2025-09-22 14:31:09 +0700

    feat: implement authentication service with login and register mutations

--- src/services/base-query.ts
+++ src/services/base-query.ts
@@ -1,4 +1,5 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { fetchBaseQuery } from "@reduxjs/toolkit/query";
+import { createApi } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL,
